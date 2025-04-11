c092bb1d13687fd9cc85c70e08863cb01db35719
Elvis Susanto <susantoelvis@gmail.com>
2025-04-11 16:39:01 +0700

    feat: added common Constants

---
+++ PokedexUIKit/Common/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  PokedexUIKit
+//
+//  Created by Elvis Susanto on 10/04/25.
+//
+
+struct Constants {
+    static let apiLimit = 10
+    static let scrollPointThreshold = 20.0
+    static let scrollEventThrottle = 500
+}
