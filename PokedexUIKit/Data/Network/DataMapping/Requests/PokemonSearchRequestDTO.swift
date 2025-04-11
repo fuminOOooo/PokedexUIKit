7fbc4b2415971914a90bf8a8dba60672e47ad6dd
Elvis Susanto <susantoelvis@gmail.com>
2025-04-11 16:40:23 +0700

    feat: added DTOs for PokemonSearch fetch

---
+++ PokedexUIKit/Data/Network/DataMapping/Requests/PokemonSearchRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonSearchRequestDTO.swift
+//  PokedexUIKit
+//
+//  Created by Elvis Susanto on 10/04/25.
+//
+
+import Foundation
+
+struct PokemonSearchRequestDTO: Encodable {
+    let query: String
+}
---
+++ PokedexUIKit/Data/Network/DataMapping/Responses/PokemonSearchResponseDTO+Mapping.swift
@@ -0,0 +1,55 @@
+//
+//  PokemonSearchResponseDTO+Mapping.swift
+//  PokedexUIKit
+//
+//  Created by Elvis Susanto on 11/04/25.
+//
+
+import Foundation
+
+struct PokemonSearchResponseDTO: Codable {
+    
+    typealias Identifier = Int
+    let id: Identifier?
+    let name: String?
+    let abilities: [Ability]?
+    let sprites: Sprites?
+    
+    init(
+        id: Int?,
+        name: String?,
+        abilities: [Ability]?,
+        sprites: Sprites?
+    ) {
+        self.id = id
+        self.name = name
+        self.abilities = abilities
+        self.sprites = sprites
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(Identifier.self, forKey: .id)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
+        self.abilities = try container.decodeIfPresent([Ability].self, forKey: .abilities)
+        self.sprites = try container.decodeIfPresent(Sprites.self, forKey: .sprites)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name, abilities, sprites
+    }
+    
+}
+
+extension PokemonSearchResponseDTO {
+    
+    func toDomain() -> Pokemon {
+        return Pokemon(
+            id: id,
+            name: name,
+            abilities: abilities,
+            sprites: sprites
+        )
+    }
+    
+}
