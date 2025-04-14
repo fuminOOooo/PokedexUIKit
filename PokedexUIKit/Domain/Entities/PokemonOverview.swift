a23c33ca6ff28f78b43b7f6605c4c20333242c84
Elvis Susanto <susantoelvis@gmail.com>
2025-04-15 02:30:31 +0700

    chore: removed private formatter from PokemonOverview entity

--- PokedexUIKit/Domain/Entities/PokemonOverview.swift
+++ PokedexUIKit/Domain/Entities/PokemonOverview.swift
@@ -13,30 +13,21 @@ struct PokemonOverview: Codable, Identifiable {
     let name: String?
     let url: String?
     
-    init(name: String, url: String) {
+    init(name: String?, url: String?) {
         self.id = UUID()
-        self.name = nameFormatter(name)
+        self.name = name
         self.url = url
     }
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.id = UUID()
-        let rawName = try container.decodeIfPresent(String.self, forKey: .name)
-        self.name = nameFormatter(rawName ?? .init())
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
         self.url = try container.decodeIfPresent(String.self, forKey: .url)
     }
     
     private enum CodingKeys: String, CodingKey {
         case name, url
     }
-    
-}
-
-private let nameFormatter: (String) -> String = { name in
-    
-    return name
-        .replacingOccurrences(of: "-", with: " ")
-        .capitalized
     
 }
