d71810d5cf29e28ae8745fe179fa775cc789a5be
Elvis Susanto <susantoelvis@gmail.com>
2025-04-15 02:27:55 +0700

    chore: updated PokemonSearchResponseDTO's abilities to AbilityDetails

--- PokedexUIKit/Data/Network/DataMapping/Responses/PokemonSearchResponseDTO+Mapping.swift
+++ PokedexUIKit/Data/Network/DataMapping/Responses/PokemonSearchResponseDTO+Mapping.swift
@@ -12,13 +12,13 @@ struct PokemonSearchResponseDTO: Codable {
     typealias Identifier = Int
     let id: Identifier?
     let name: String?
-    let abilities: [Ability]?
+    let abilities: [AbilityDetails]?
     let sprites: Sprites?
     
     init(
         id: Int?,
         name: String?,
-        abilities: [Ability]?,
+        abilities: [AbilityDetails]?,
         sprites: Sprites?
     ) {
         self.id = id
@@ -31,7 +31,7 @@ struct PokemonSearchResponseDTO: Codable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.id = try container.decodeIfPresent(Identifier.self, forKey: .id)
         self.name = try container.decodeIfPresent(String.self, forKey: .name)
-        self.abilities = try container.decodeIfPresent([Ability].self, forKey: .abilities)
+        self.abilities = try container.decodeIfPresent([AbilityDetails].self, forKey: .abilities)
         self.sprites = try container.decodeIfPresent(Sprites.self, forKey: .sprites)
     }
     
@@ -46,7 +46,7 @@ extension PokemonSearchResponseDTO {
     func toDomain() -> Pokemon {
         return Pokemon(
             id: id,
-            name: name,
+            name: name.formattedForName(),
             abilities: abilities,
             sprites: sprites
         )
